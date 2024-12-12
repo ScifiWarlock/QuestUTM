298fe68bbeb4ac4985d1d244c5dd0512784a2700
rakmot <rakmot19@gmail.com>
2024-12-11 21:45:39 -0800

    added logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
 import requests
 import pandas as pd
+import json
+from streamlit_lottie import st_lottie
+import os
 
-st.title("QuestUTM: WildfireQuest's intranet traffic management system")
+file_path = os.getcwd() + '\logo.json'
+
+with open(file_path, "r") as file:
+    url = json.load(file)
+
+st.title("VCHS WildfireQuest")
+
+st_lottie(url,
+          reverse=True,
+          height=200,
+          width=400,
+          speed=1,
+          loop=True,
+          quality='high',
+          key='logo')
 
 table = requests.get("http://127.0.0.1:8000/table_send").json()
 
